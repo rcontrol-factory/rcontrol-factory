955728e138e5eba65af59cca79eeddfb8de695af
rcontrol-factory <tevesjulio16@gmail.com>
2026-02-08 14:53:08 -0500

    Update router.js

--- app/js/router.js
+++ app/js/router.js
@@ -1,1 +1,13 @@
-// simple router
+import { templates } from "./templates.js";
+
+export function navigate(route) {
+  const app = document.getElementById("app");
+
+  if (!app) return;
+
+  if (route === "newapp") {
+    app.innerHTML = templates.newApp;
+  } else {
+    app.innerHTML = templates.home;
+  }
+}
