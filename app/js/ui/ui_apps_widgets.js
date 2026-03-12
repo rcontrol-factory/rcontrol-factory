e184861b8f510a335814f05c04ebea6e1589c0f1
rcontrol-factory <tevesjulio16@gmail.com>
2026-03-12 10:55:29 -0400

    Create ui_apps_widgets.js

---
+++ app/js/ui/ui_apps_widgets.js
@@ -0,0 +1,10 @@
+(() => {
+  "use strict";
+
+  window.RCF_UI = window.RCF_UI || {};
+
+  window.RCF_UI.nomeDoModulo = window.RCF_UI.nomeDoModulo || {
+    init() {},
+    render() { return ""; }
+  };
+})();
